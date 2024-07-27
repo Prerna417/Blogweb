5700e538fc47c31c218103895ccc00e8f0a1b57b
Prerna417 <b421036@iiit-bh.ac.in>
2024-07-27 08:56:44 +0530

    port failure

--- api/index.js
+++ api/index.js
@@ -38,7 +38,9 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+console.log('Environment Variables:', process.env);
 
-app.listen("5000", () => {
-    console.log("backend is running")
-})
+const port = process.env.PORT||5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
