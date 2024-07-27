461ec15495aebba3c817a003e9c7a4e06ec3581f
Prerna417 <b421036@iiit-bh.ac.in>
2024-07-27 09:15:57 +0530

    added netlify link

--- api/index.js
+++ api/index.js
@@ -10,7 +10,13 @@ const multer = require("multer")
 const path = require("path")
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'https://melodic-beignet-a0429e.netlify.app/', // Replace with your Netlify domain
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    }
+));
 
 dotenv.config();
 app.use(express.json());
@@ -43,7 +49,7 @@ app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 console.log('Environment Variables:', process.env);
 
-const port = process.env.PORT;
+const port = process.env.PORT||5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
